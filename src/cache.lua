b000c08e4693f0fe4026468b0096482b11e19cec
William Cummings <wcummings@forensiq.com>
2015-11-16 14:56:07 -0500

    Dont need get_all

--- src/cache.lua
+++ src/cache.lua
@@ -34,16 +34,6 @@ function cache.new()
     function self.flush_expired()
     end
 
-    function self.get_all()
-        local items = {}
-        
-        for _, v in pairs(self.data) do
-            items.insert(v)
-        end
-        
-        return items
-    end
-
     return self
 
 end
